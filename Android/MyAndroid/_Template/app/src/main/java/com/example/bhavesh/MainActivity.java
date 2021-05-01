88306a7c5a52daafef6172bfd76c73d83e6c5e32
Bhavesh Joshi <bhaveshjoshi934@gmail.com>
2021-05-01 08:29:59 +0530

    Android Assignmnts

---
+++ Android/MyAndroid/_Template/app/src/main/java/com/example/bhavesh/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.bhavesh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    System.out.println("BDJ: Hello Bhavesh!!!");
+
+    }
+}
