528e356fc03098ed6c184b081e5b3a3ce140ecd0
Bhavesh Joshi <bhaveshjoshi934@gmail.com>
2021-05-06 23:33:52 +0530

    Android Assignments

---
+++ Android/MyAndroid/6.Perspective_Colored_Triangle/app/build/generated/source/buildConfig/debug/com/example/colored/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.colored;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.colored";
+  public static final String BUILD_TYPE = "debug";
+}
---
+++ Android/MyAndroid/8.Two_2D_Shapes_Rotating/app/build/generated/source/buildConfig/release/com/example/twoshapesrotating/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.twoshapesrotating;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.example.twoshapesrotating";
+  public static final String BUILD_TYPE = "release";
+}
